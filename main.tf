8f9bd6ae17337c2dda1a605556bc3f6652521ccc
Andy Thompson <athompson@redapt.com>
2017-07-10 16:10:58 -0700

    test

--- main.tf
+++ main.tf
@@ -83,8 +83,6 @@ resource "azurerm_virtual_machine" "vm" {
   }
 }
 
-/*
-
 resource "azurerm_public_ip" "lbpip" {
   name                         = "${var.rg_prefix}-ip"
   location                     = "${var.location}"
@@ -145,6 +143,3 @@ resource "azurerm_lb_probe" "lb_probe" {
   interval_in_seconds = 5
   number_of_probes    = 2
 }
-
-*/
-
--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,6 @@ output "hostname" {
   value = "${var.hostname}"
 }
 
-/*
-
 output "vm_fqdn" {
   value = "${azurerm_public_ip.lbpip.fqdn}"
 }
@@ -11,5 +9,3 @@ output "vm_fqdn" {
 output "ssh_command" {
   value = "ssh ${var.admin_username}@${azurerm_public_ip.lbpip.fqdn}"
 }
-
-*/
